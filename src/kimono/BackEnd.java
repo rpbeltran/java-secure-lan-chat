d46efcdcdf65f0544f728e4a306782205d3132dc
Nolan Groves <ng31415@gmail.com>
2015-04-02 13:39:48 -0500

    BackEnd

--- src/kimono/BackEnd.java
+++ src/kimono/BackEnd.java
@@ -1,5 +1,43 @@
 package kimono;
 
-public class BackEnd {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class BackEnd {
+	
+	private HashMap<String, List<String>> theData;
+	private Encoder encoder;
+	
+	public BackEnd(String filename) {
+		theData = new HashMap<String, List<String>>();
+		Encoder encoder = new Encoder(filename);
+		load();
+	}
+	
+	public List<String> getMessages(String s) {
+		return theData.get(s);
+	}
+	
+	public void addMessage(String user, String msg) {
+		if (!theData.keySet().contains(word)) {
+			theData.put(user, new ArrayList<String>());
+		}
+		theData.get(user).add(msg);
+		save();
+	}
+	
+	public List<String> getUsers() {
+		ArrayList<String> userList = new ArrayList<String>();
+		userList.addAll(theData.keySet());
+		return userList;
+	}
+
+	public void save() {
+		encoder.write(theData);
+	}
+	
+	public void load() {
+		theData = encoder.read();
+	}
 }
