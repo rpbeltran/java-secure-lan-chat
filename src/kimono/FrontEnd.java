d93323862f2083264789e8022bb383d7665418d0
lekro <meesayousa@gmail.com>
2015-04-02 23:02:06 -0500

    Add menu box to select operation upon application start.

--- src/kimono/FrontEnd.java
+++ src/kimono/FrontEnd.java
@@ -18,7 +18,28 @@ public class FrontEnd {
 		
 		JFrame frame = new JFrame("Closed Kimono ver. "+Kimono.VERSION);
 		
-		Box vBox = Box.createVerticalBox();
+		// First, set up a box for the functionality of the menu.
+		
+		Box initBox = Box.createVerticalBox();
+		JButton save = new JButton("Save a message");
+		JButton load = new JButton("Load a message");
+		
+		save.setPreferredSize(new Dimension(320,90));
+		load.setPreferredSize(new Dimension(320,90));
+		
+		save.setMaximumSize(new Dimension(320,90));
+		load.setMaximumSize(new Dimension(320,90));
+		
+		initBox.add(save);
+		initBox.add(load);
+		
+		// Then, one for the loading screen.
+		
+		Box loadBox = Box.createVerticalBox();
+		
+		// And finally, one for the save screen.
+		
+		Box saveBox = Box.createVerticalBox();
 		Box userBox = Box.createHorizontalBox();
 		JLabel userLabel = new JLabel("Username:");
 		userBox.add(userLabel);
@@ -47,6 +68,21 @@ public class FrontEnd {
 		JButton saveButton = new JButton("Save");
 		JButton backButton = new JButton("Back");
 		
+		saveBox.add(userBox);
+		saveBox.add(userField);
+		saveBox.add(passBox);
+		saveBox.add(passField);
+		saveBox.add(messBox);
+		saveBox.add(messField);
+		
+		Box hBox = Box.createHorizontalBox();
+		hBox.add(saveButton);
+		hBox.add(backButton);
+		hBox.add(Box.createHorizontalGlue());
+		saveBox.add(hBox);
+		
+		// Set up action listener for the buttons
+		
 		ActionListener al = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
@@ -59,28 +95,27 @@ public class FrontEnd {
 					//execute save code here
 					break;
 				case "Back":
-					//go to previous window / screen
+					frame.remove(saveBox);
+					frame.remove(loadBox);
+					frame.add(initBox);
+					frame.pack();
+					break;
+				case "Save a message":
+					frame.remove(initBox);
+					frame.add(saveBox);
+					frame.pack();
+					break;
+				case "Load a message":
 				}
 			}
 		};
 		
 		saveButton.addActionListener(al);
 		backButton.addActionListener(al);
+		save.addActionListener(al);
+		load.addActionListener(al);
 		
-		vBox.add(userBox);
-		vBox.add(userField);
-		vBox.add(passBox);
-		vBox.add(passField);
-		vBox.add(messBox);
-		vBox.add(messField);
-		
-		Box hBox = Box.createHorizontalBox();
-		hBox.add(saveButton);
-		hBox.add(backButton);
-		hBox.add(Box.createHorizontalGlue());
-		vBox.add(hBox);
-		
-		frame.add(vBox);
+		frame.add(initBox);
 		frame.pack();
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
