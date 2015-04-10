94f84976b4083fb9e80bcc10e575d26df535f166
legomaniack <ng31415@gmail.com>
2015-04-10 13:15:07 -0500

    Work on CommandLine

--- src/kimono/CommandLine.java
+++ src/kimono/CommandLine.java
@@ -3,70 +3,97 @@ package kimono;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class CommandLine {
 
 	BufferedReader br;
 	BackEnd backend;
+	private static HashMap<String, String> commands;
 	
-	
-	public CommandLine(boolean server) {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Username: ");
-		String u = input();
-		System.out.print("Password: ");
-		String p = input();
-		System.out.print("Server IP: ");
-		String host = input();
-		System.out.print("Server port: ");
-		int port = Integer.parseInt(input());
-		
-		if (server) {
-			//Start headless server
-		} else {
-			backend = new BackEnd(u, p, host, port);
-			runClient();
-		}
-	}
-	
-	public CommandLine(boolean server, String host, int port) {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Username: ");
-		String u = input();
-		System.out.print("Password: ");
-		String p = input();
-		
-		if (server) {
-			//Start headless server
-		} else {
-			backend = new BackEnd(u, p, host, port);
-			runClient();
-		}
-		
-	}
-	
-	public CommandLine(boolean server, String u, String p, String host, int port) {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		if (server) {
-			//Start headless server
-		} else {
-			backend = new BackEnd(u, p, host, port);
-			runClient();
-		}
-		
-	}
-	
-	private void runClient(){
+	public CommandLine(boolean server,String u, String p, String host, int port) {
 		System.out.println();
+		br = new BufferedReader(new InputStreamReader(System.in));
+		while (u.equals("")) {
+			System.out.print("Username: ");
+			u = input();
+		}
+		while (p.equals("")) {
+			System.out.print("Password: ");
+			p = input();
+		}
+		/*		// Handle Defaults in Main
+		if (host.equals("")) {
+			System.out.print("Hostname: ");
+			host = input();
+			if (host.equals("")) host = "127.0.0.1";
+		}
+		if (port == 0) {
+			System.out.print("Hostname: ");
+			String porti = input();
+			if (porti.equals("")) {
+				port = 32800;
+			} else {
+				port = Integer.parseInt(porti);
+			}
+		}*/
+		
+		setUpCommands();
+		
+		if (server) {
+			//Start headless server
+			runClient(true);
+		} else {
+			backend = new BackEnd(u, p, host, port);
+			runClient(false);
+		}
+		
+	}
+	
+	private void runClient(boolean admin){
+		System.out.println("Welcome to Closed Kimono");
+		System.out.println();
+		help();
+		System.out.println("Avaliable rooms: ");
+		//Display available rooms
 		//Somehow make CLI here
 	}
+	
+	private boolean runCommand(String cmd, boolean admin) {
+		switch (cmd) 
+		{
+		case "help":
+			help();
+			return true;
+		default:
+			return false;
+		}
+	}
+	
+	private void setUpCommands() {
+		commands = new HashMap<String, String>();
+		commands.put("help", "Displays help");
+		commands.put("say", "Send a message");
+		commands.put("room", "Create or join a room");
+		commands.put("rooms", "List avaliable rooms");
+		commands.put("close", "Deletes a room if you are the owner");
+	}
+	
+	private void help() {
+		System.out.println("Help:");
+		for(String s: commands.keySet()) {
+			System.out.println("\t!"+s+" - "+commands.get(s));
+		}
+		System.out.println();
+	}
+	
 	
 	public String input() {
 		String r = new String();
 		try {
 			r = br.readLine();
 		} catch (IOException ioe) {
-			System.out.println("IO error trying to read your name!");
+			System.out.println("IO error!");
 	        System.exit(1);
 	    }
 		return r;
